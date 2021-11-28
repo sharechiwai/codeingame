2b7f8b0e9f794bcad64e7be471391fd56846d67a
Chiwai Yau <chiwai.yau@gravitysupplychain.com>
2021-11-28 10:06:26 +0800

    unsolved

---
+++ javascript/clash/water-level.js
@@ -0,0 +1,13 @@
+// Line 1: An integer n denotes the number of different levels
+// Line 2: An integer m the number of readings from sensor
+// Line 3: m space separated integer reading. A reading of -1 means water level is unspecified (i.e., water surface is between two levels).
+
+//Output
+// One line having m space separated integer water_level for corresponding sensor reading. If any reading can not specify the water level you have to print previous water_level
+
+
+// Constraints
+// 3 ≤ n, m ≤ 100
+// -1 ≤ reading ≤ n-1; 1st reading will never be -1
+// 0 ≤ water_level ≤ n-1
+
